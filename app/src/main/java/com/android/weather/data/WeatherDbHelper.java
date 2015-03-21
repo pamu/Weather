dc26a6ef0fa9a0ce83527c1ada7c7a382f44e48c
pamu <nagarjuna.pamu@gmail.com>
2015-03-21 21:50:17 +0530

    data

--- app/src/main/java/com/android/weather/data/WeatherDbHelper.java
+++ app/src/main/java/com/android/weather/data/WeatherDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.android.weather.data.WeatherContract.WeatherEntry;
+
 /**
  * Created by android on 21/3/15.
  */
@@ -18,7 +20,13 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        final String SQL_CREATE_WEATHER_TABLE =
+                "CREATE TABLE " + WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
+                        WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL " +
+                        WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL " +
+                        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL " +
+                        WeatherEntry.COLUMN_MIN_TEMP + " ";
     }
 
     @Override
